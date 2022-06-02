a1e7ee0a00915576a571a0b1dee91860a43c20ee
Tharlei Aleixo <tharlei.aleixo@grupofocus.com.br>
2022-06-02 09:28:15 -0300

    feat: add dashboard color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        cyan: '#5BBFBA',
+        purple: '#5F6CAF',
+        yellow: '#E7F0C3',
+        orange: '#F0CF85',
+        green: '#A4D4AE',
+        gray: {
+          100: '#EDF1F8',
+          500: '#E6E7E7',
+          700: '#CCD0DC',
+          900: '#BABBBB',
+        },
+        white: '#FFFEFE',
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
