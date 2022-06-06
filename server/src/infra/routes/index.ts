ab23e782e19925b26f2110a73458d5e4b44ba1d1
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-06-05 21:58:08 -0300

    feat: add routes
    - Route to list transportations
    - Route to store quote

--- server/src/infra/routes/index.ts
+++ server/src/infra/routes/index.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
-import { ExampleController } from '../controllers/ExampleController';
+import { QuoteController } from '../controllers/QuoteController';
+import { TransportController } from '../controllers/TransportController';
+
+import { StoreQuoteRequest } from '../controllers/requests/StoreQuoteRequest';
+import { TokenMiddleware } from '../middlewares/TokenMiddleware';
 
 const routes = Router();
 
-routes.get('/examples', new ExampleController().index);
+routes.use('/', TokenMiddleware);
+routes.get('/transports', new TransportController().index);
+routes.post('/quotes', StoreQuoteRequest, new QuoteController().store);
 
 export { routes };
