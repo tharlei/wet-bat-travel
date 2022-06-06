eeab7d5de327225bf80142908fad26713a16314a
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-06-05 22:16:39 -0300

    feat: add security token in api requests

--- src/infra/api.ts
+++ src/infra/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
-import { VITE_API_URL } from '../config';
+import { API_URL, API_TOKEN } from '../config';
 
 const API = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: API_URL,
+  headers: {
+    Authorization: API_TOKEN,
+  },
 });
 
 export { API };
