79a41521b44ee9842986bf0e559d02571287dd23
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-06-05 22:18:10 -0300

    fix: duplicate use effect in mount component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import { Dashboard } from './layouts/Dashboard';
@@ -8,13 +7,11 @@ import './global.scss';
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <Dashboard>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Dashboard>
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <Dashboard>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Dashboard>
+  </BrowserRouter>,
 );
